f27e5b46c0b11ddfa263323cdf7e980fb2e8a12d
df@ <@>
2020-10-28 17:07:48 +0000

    started ghostname model; made migration

--- PhanGon/phantomname/models.py
+++ PhanGon/phantomname/models.py
@@ -5,3 +5,10 @@ from django.contrib import auth
 
 class User(auth.models.AbstractUser):
     pass
+
+
+
+
+class GhostName(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=500)
