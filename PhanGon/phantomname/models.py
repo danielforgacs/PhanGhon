8108fa81cd710ff42a616e64a1b10c4f83ee01cd
df@ <@>
2020-10-28 17:42:17 +0000

    started Phantom name model; made migrations

--- PhanGon/phantomname/models.py
+++ PhanGon/phantomname/models.py
@@ -12,3 +12,10 @@ class User(auth.models.AbstractUser):
 class GhostName(models.Model):
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=500)
+
+
+
+
+class PhantomName(models.Model):
+    user = models.ForeignKey(to='User', on_delete=models.CASCADE)
+    ghostname = models.ForeignKey(to='GhostName', on_delete=models.CASCADE)
