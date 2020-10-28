70be4fb364b017d06110fd78e18691cf543d3850
df@ <@>
2020-10-28 15:26:54 +0000

    added user to admin

--- PhanGon/phantomname/admin.py
+++ PhanGon/phantomname/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+
+@admin.register(models.User)
+class User(admin.ModelAdmin):
+    list_display = (
+        'username',
+        'is_staff',
+        'is_superuser',
+    )
