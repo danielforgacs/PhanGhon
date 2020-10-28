6c1880a9e9a996986d3cdd2d9969330c9fa10866
df@ <@>
2020-10-28 15:38:56 +0000

    wip register

--- PhanGon/phantomname/views.py
+++ PhanGon/phantomname/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
+from . import forms
 
 
 
 
 def index(request):
     template = 'phantomname/index.html'
-    context = {}
+    userform = forms.UserForm()
+    context = {
+        'userform': userform,
+    }
     response = render(
         request=request,
         template_name=template,
