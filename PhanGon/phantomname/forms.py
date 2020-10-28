9232412d3bc84e4e79221bebd493f6d0d3bb9958
df@ <@>
2020-10-28 15:38:31 +0000

    started forms

---
+++ PhanGon/phantomname/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from . import models
+
+
+
+
+class UserForm(forms.ModelForm):
+    class Meta:
+        model = models.User
+        fields = (
+            'username',
+            'password',
+        )
