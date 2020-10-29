25745ad3e742604db9eca3057325b044091f83a6
df@ <@>
2020-10-29 23:14:18 +0000

    wrote test

--- PhanGhon/phantomname/tests/test_views.py
+++ PhanGhon/phantomname/tests/test_views.py
@@ -1,6 +1,12 @@
+import re
 import pytest
 from django import urls
 from phantomname import views
+from phantomname import models
+
+
+TEST_GHOST_NAME_COUNT = 5
+TEST_GHOST_NAME_TEMPLATE = 'POKL'
 
 
 
@@ -10,3 +16,25 @@ def test_index(client):
     url = urls.reverse('index')
     response = client.get(url)
     assert response.status_code == 200
+
+
+
+
+@pytest.fixture
+def data01():
+    for row in range(TEST_GHOST_NAME_COUNT):
+        idx = '{:04d}'.format(row)
+        name = TEST_GHOST_NAME_TEMPLATE + idx
+        models.GhostName.objects.create(name=name)
+
+
+
+
+@pytest.mark.django_db
+def test_index_lists_ghostnames(client, data01):
+    url = urls.reverse('index')
+    response = client.get(url)
+    html = response.content.decode()
+    pattern = TEST_GHOST_NAME_TEMPLATE+r'\d{4}'
+    found = re.findall(pattern, html)
+    assert len(found) == TEST_GHOST_NAME_COUNT
